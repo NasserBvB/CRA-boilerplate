f486c5d4c9d7fe51b0d949e67b75fe533145d2a3
Nasser Setti <abdennasseressati@gmail>
2020-10-18 23:17:53 +0100

    chore: fix pages roles

--- src/routes/Authenticated.jsx
+++ src/routes/Authenticated.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
+import { useUser } from 'features/auth/providers/UserProvider'
 import history from 'helpers/history'
 import Header from 'ui/components/Header'
 import Article from 'features/dashboard/modules/article'
@@ -10,12 +11,24 @@ import Facture from 'features/dashboard/modules/facture'
 const NotFound = () => <h2>404 Not Found</h2>
 
 export default () => {
+  const { user } = useUser()
+
   return (
     <Router history={history}>
       <Header />
       <Switch>
-        <Route path="/" exact component={Utilisateur} />
-        <Route path="/utilisateur" exact component={Utilisateur} />
+        <Route
+          path="/"
+          exact
+          component={
+            user.data && user.data.profil && user.data.profil.id === 1
+              ? Utilisateur
+              : Article
+          }
+        />
+        {user.data && user.data.profil && user.data.profil.id === 1 && (
+          <Route path="/utilisateur" exact component={Utilisateur} />
+        )}
         <Route path="/article" exact component={Article} />
         <Route path="/client" exact component={Client} />
         <Route path="/facture" exact component={Facture} />
